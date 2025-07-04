54343f7c94889e2c0c8a564088cad93581713afe
valentinmadiot <valentin.madiot@gmail.com>
2025-07-04 20:02:14 +0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const sliderMain = new Swiper(".slider__main", {
+  freeMode: true,
+  centeredSlides: true,
+  mousewheel: true,
+  parallax: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60,
+    },
+  },
+});
+
+const sliderBg = new Swiper(".slider__bg", {
+  centeredSlides: true,
+  parallax: true,
+  spaceBetween: 60,
+  slidesPerView: 3.5,
+});
+
+sliderMain.controller.control = sliderBg;
+
+document.querySelectorAll(".slider__item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+let content = document.querySelector(".slider__content");
+sliderMain.on("slideChange", (e) => {
+  sliderMain.activeIndex > 0
+    ? content.classList.add("hidden")
+    : content.classList.remove("hidden");
+});
